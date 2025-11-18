617a40030284b6f0975cd5c23c5faffbbb95634e
Shoaib Akhter <shoaib3183@gmail.com>
2025-11-18 22:45:14 +0530

    Added haadish.site to CORS

--- api/server.js
+++ api/server.js
@@ -11,7 +11,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'https://www.haadish.site',
-  'http://www.haadish.site'
+  'http://www.haadish.site',
+  'http://haadish.site'
 ];
 
 // Enable CORS
